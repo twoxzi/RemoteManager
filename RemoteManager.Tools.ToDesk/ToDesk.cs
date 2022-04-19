f2aebd5c4ec3fe9ed88e2d2cfae95e42b8fc8956
twoxzi <twoxzi@foxmail.com>
2022-04-19 15:56:22 +0800

    增加ToDesk远程控制

---
+++ RemoteManager.Tools.ToDesk/ToDesk.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Twoxzi.RemoteManager.Entity;
+using Twoxzi.RemoteManager.Tools;
+
+namespace RemoteManager.Tools
+{
+    [RemoteTool(ToolCode =ToDesk.ToolCode,ToolName ="ToDesk",Memo ="ToDesk桌面远程")]
+    public class ToDesk : RemoteToolBase
+    {
+        const String ToolCode = "ToDesk";
+        public override void Open(RemoteInfo info)
+        {
+            String exe = GetExePath("ToDesk");
+            if (String.IsNullOrEmpty(exe))
+            {
+                throw new Exception("未找到工具ToDesk，请确保正确安装，启动已中止");
+            }
+            exe = Path.Combine(exe, "ToDesk.exe");
+            Process.Start(exe, $"-control -id {info.ID} -passwd {info.Password}");
+        }
+    }
+}
